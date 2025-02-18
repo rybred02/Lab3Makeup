5ff6ed31cd21aa60fa7c001457d1ed0d5583108a
rybred02 <rcbrede@ilstu.edu>
2025-02-18 15:17:45 -0600

    Lab3IfElseStatement
    Post-working

---
+++ sketch_feb18a/sketch_feb18a.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A6, INPUT);
+int lightLevel = analogRead(A6);
+Serial.begin(9600);
+Serial.println(lightLevel);
+if (lightLevel < 100) {
+  Serial.println("It's really dark!");
+}
+else if (lightLevel < 200) {
+  Serial.println("It's dim in here");
+}
+else if (lightLevel < 700) {
+  Serial.println("It's a little bright!");
+}
+else if (lightLevel < 1024) {
+  Serial.println("It's really bright!");
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
